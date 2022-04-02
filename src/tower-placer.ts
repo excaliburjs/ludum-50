1b90427699d432df30d1840aa82d71511d47db43
Erik Onarheim <erik.onarheim@gmail.com>
2022-04-02 11:31:30 -0500

    Add highlight

--- src/tower-placer.ts
+++ src/tower-placer.ts
@@ -1,17 +1,32 @@
-import { Engine, Input, Tile } from "excalibur";
+import { Engine, Input, Tile, Actor, Color, Rectangle, vec } from "excalibur";
 import { Tower } from "./tower";
 import { Grid } from "./grid";
+import config from "./config";
 
 
 export class TowerPlacer {
     private _grid: Grid;
     private _engine: Engine;
     private _highlightedTile: Tile | null = null;
+    private _highlight: Actor;
     constructor(grid: Grid, engine: Engine) {
         this._grid = grid;
         this._engine = engine;
         engine.input.pointers.primary.on('move', (evt) => this.onPointerMove(evt));
         engine.input.pointers.primary.on('down', () => this.onConfirm());
+        this._highlight = new Actor({
+            pos: vec(-100, -100) // default offscreen
+        })
+        const rect = new Rectangle({
+            width: config.tileWidth,
+            height: config.tileHeight,
+            color: Color.Transparent,
+            strokeColor: Color.Green,
+            lineWidth: 3,
+            lineDash: [3, 3]
+        })
+        this._highlight.graphics.use(rect);
+        engine.add(this._highlight);
     }
 
     onPointerMove(evt: Input.PointerEvent) {
@@ -20,6 +35,7 @@ export class TowerPlacer {
 
         if (maybeTile) {
             this._highlightedTile = maybeTile;
+            this._highlight.pos = this._highlightedTile.pos.add(vec(config.tileWidth/2, config.tileHeight/2));
         }
     }
 
