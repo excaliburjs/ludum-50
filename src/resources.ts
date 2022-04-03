c749b984faf9d00ebd00506c6d54a3eb9fed1082
Erik Onarheim <erik.onarheim@gmail.com>
2022-04-03 18:42:54 -0500

    Add mp3's for safari

--- src/resources.ts
+++ src/resources.ts
@@ -19,10 +19,15 @@ import clankUrl from "./sounds/clank.wav";
 import bgAmbiance from "./sounds/bg-beach.mp3";
 import bgMusic from "./sounds/bg-music.mp3";
 import waveAppear from "./sounds/wave-appear.ogg";
+import waveAppearMp3 from "./sounds/wave-appear.mp3";
 import impactEnemyRock from "./sounds/impact-enemy-rock.ogg";
+import impactEnemyRockMp3 from "./sounds/impact-enemy-rock.mp3";
 import impactTower from "./sounds/impact-tower.ogg";
+import impactTowerMp3 from "./sounds/impact-tower.mp3";
 import impactCastle from "./sounds/impact-castle.ogg";
+import impactCastleMp3 from "./sounds/impact-castle.mp3";
 import pickTower from "./sounds/pick-tower.ogg";
+import pickTowerMp3 from "./sounds/pick-tower.mp3";
 import placeTower from "./sounds/place-tower.mp3";
 import fling01 from "./sounds/fling-01.mp3";
 import fling02 from "./sounds/fling-02.mp3";
@@ -53,10 +58,10 @@ export const Resources = {
   Clank: new Sound(clankUrl),
   BackgroundAmbiance: new Sound(bgAmbiance),
   BackgroundMusic: new Sound(bgMusic),
-  FxWaveAppear: new Sound(waveAppear),
-  FxImpactEnemyRock: new Sound(impactEnemyRock),
-  FxImpactTowerByEnemy: new Sound(impactTower),
-  FxImpactCastleByEnemy: new Sound(impactCastle),
+  FxWaveAppear: new Sound(waveAppearMp3, waveAppear),
+  FxImpactEnemyRock: new Sound(impactEnemyRockMp3, impactEnemyRock),
+  FxImpactTowerByEnemy: new Sound(impactTowerMp3, impactTower),
+  FxImpactCastleByEnemy: new Sound(impactCastleMp3, impactCastle),
   FxPlaceTower: new Sound(placeTower),
   FxTowerFlings01: new Sound(fling01),
   FxTowerFlings02: new Sound(fling02),
@@ -64,7 +69,7 @@ export const Resources = {
   FxTowerFlings04: new Sound(fling04),
   FxTowerFlings05: new Sound(fling05),
   FxTowerFlings06: new Sound(fling06),
-  UiPickTower: new Sound(pickTower),
+  UiPickTower: new Sound(pickTowerMp3, pickTower),
 };
 
 export const FxTowerFlings = [
